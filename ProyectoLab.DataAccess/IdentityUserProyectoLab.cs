4168693dd05bca468e25869d1745bba2eee60e97
Gustavo <gusstavocta@gmail.com>
2024-10-18 23:44:21 -0500

    implementaciones

--- ProyectoLab.DataAccess/IdentityUserProyectoLab.cs
+++ ProyectoLab.DataAccess/IdentityUserProyectoLab.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProyectoLab.DataAccess
+// IdentityUserProyectoLab hereda de IdentityUser
 {
     public class IdentityUserProyectoLab : IdentityUser
     {
